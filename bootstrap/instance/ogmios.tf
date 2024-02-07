2a4808dfa82d723e848127d033034494fda3233b
Felipe Gonzalez <gonzalezz_felipe@hotmail.com>
2024-02-07 14:53:56 -0300

    chore: Ogmios instance bootstrap
    * chore: Add ogmios instance, configs, and service bootstrap
    
    * Add namespace to instance deployment and fix local name
    
    * Match service using network and version labels
    
    * Remove unused local
    
    * Fix service name and labels
    
    * Add feature with namespace
    
    * Use local.name on service

---
+++ bootstrap/instance/ogmios.tf
@@ -0,0 +1,135 @@
+locals {
+  name           = "ogmios-${var.network}-${var.ogmios_version}-${var.salt}"
+  image          = "ghcr.io/demeter-run/cardano-ogmios:${var.ogmios_image_tag}"
+  container_port = 1337
+}
+
+resource "kubernetes_deployment_v1" "ogmios" {
+  metadata {
+    name      = local.name
+    namespace = var.namespace
+    labels = {
+      "demeter.run/kind"                   = "OgmiosInstance"
+      "cardano.demeter.run/network"        = var.network
+      "cardano.demeter.run/ogmios-version" = var.ogmios_version
+    }
+  }
+  spec {
+    replicas = var.replicas
+    selector {
+      match_labels = {
+        "demeter.run/instance"               = local.name
+        "cardano.demeter.run/network"        = var.network
+        "cardano.demeter.run/ogmios-version" = var.ogmios_version
+      }
+    }
+    template {
+      metadata {
+        name = local.name
+        labels = {
+          "demeter.run/instance"               = local.name
+          "cardano.demeter.run/network"        = var.network
+          "cardano.demeter.run/ogmios-version" = var.ogmios_version
+        }
+      }
+      spec {
+        restart_policy = "Always"
+
+        security_context {
+          fs_group = 1000
+        }
+
+        container {
+          name              = "main"
+          image             = local.image
+          image_pull_policy = "IfNotPresent"
+          args = [
+            "--node-socket", "/ipc/node.socket",
+            "--node-config", "/config/config.json",
+            "--host", "0.0.0.0"
+          ]
+
+          resources {
+            limits = {
+              cpu    = var.resources.limits.cpu
+              memory = var.resources.limits.memory
+            }
+            requests = {
+              cpu    = var.resources.requests.cpu
+              memory = var.resources.requests.memory
+            }
+          }
+
+          port {
+            container_port = local.container_port
+            name           = "api"
+            protocol       = "TCP"
+          }
+
+          volume_mount {
+            name       = "ipc"
+            mount_path = "/ipc"
+          }
+
+          volume_mount {
+            name       = "node-config"
+            mount_path = "/config"
+          }
+        }
+
+        container {
+          name  = "socat"
+          image = "alpine/socat"
+          args = [
+            "UNIX-LISTEN:/ipc/node.socket,reuseaddr,fork,unlink-early",
+            "TCP-CONNECT:${var.node_private_dns}"
+          ]
+
+          security_context {
+            run_as_user  = 1000
+            run_as_group = 1000
+          }
+
+          volume_mount {
+            name       = "ipc"
+            mount_path = "/ipc"
+          }
+        }
+
+        volume {
+          name = "ipc"
+          empty_dir {}
+        }
+
+        volume {
+          name = "node-config"
+
+          config_map {
+            name = "configs-${var.network}"
+          }
+        }
+
+        toleration {
+          effect   = "NoSchedule"
+          key      = "demeter.run/compute-profile"
+          operator = "Equal"
+          value    = "general-purpose"
+        }
+
+        toleration {
+          effect   = "NoSchedule"
+          key      = "demeter.run/compute-arch"
+          operator = "Equal"
+          value    = "x86"
+        }
+
+        toleration {
+          effect   = "NoSchedule"
+          key      = "demeter.run/availability-sla"
+          operator = "Equal"
+          value    = "consistent"
+        }
+      }
+    }
+  }
+}
---
+++ bootstrap/service/main.tf
@@ -0,0 +1,29 @@
+variable "namespace" {
+  description = "the namespace where the resources will be created"
+}
+
+variable "network" {
+  description = "cardano node network"
+
+  validation {
+    condition     = contains(["mainnet", "preprod", "preview"], var.network)
+    error_message = "Invalid network. Allowed values are mainnet, preprod and preview."
+  }
+}
+
+variable "ogmios_version" {
+  type = string
+
+  validation {
+    condition     = contains(["5", "6"], var.ogmios_version)
+    error_message = "Invalid version. Allowed values are 5 or 6."
+  }
+}
+
+variable "salt" {
+  type = string
+}
+
+variable "well_known_service" {
+  default = true
+}
